31008c1b39a74f61d5f4eaa554ba61f34693ba41
Dianciré Diallo <diaancir@gmail.com>
2024-05-15 12:48:15 +0200

    feat(hero): modification hero section and disabled rules

--- components/frontend/Hero.tsx
+++ components/frontend/Hero.tsx
@@ -24,7 +24,28 @@ function Hero() {
                 Facilitez la prise de rendez-vous médicaux avec Docto-Rdv ! Localisez rapidement vos médecins et planifiez vos consultations en toute simplicité. Simplifiez votre accès aux soins médicaux dès maintenant !
                 </p>
                 <SearchBar/>
-                <Button className='mt-4'>Explorez maintenant</Button>
+                <Button className='mt-4'>
+                  Besoin d'un médecin de toute urgence
+                </Button>
+                <div className="flex items-center justify-center mt-10 space-x-6 lg:justify-start sm:space-x-8">
+                    <div className="flex items-center">
+                        <p className="text-3xl font-medium text-gray-900 sm:text-4xl font-pj">800</p>
+                        <p className="ml-3 text-sm text-gray-900 font-pj">Spécialistes<br />Actifs</p>
+                    </div>
+                    <div className="hidden sm:block">
+                        <svg className="text-gray-400" width="16" height="39" viewBox="0 0 16 39" fill="none" stroke="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <line x1="0.72265" y1="10.584" x2="15.7226" y2="0.583975"></line>
+                            <line x1="0.72265" y1="17.584" x2="15.7226" y2="7.58398"></line>
+                            <line x1="0.72265" y1="24.584" x2="15.7226" y2="14.584"></line>
+                            <line x1="0.72265" y1="31.584" x2="15.7226" y2="21.584"></line>
+                            <line x1="0.72265" y1="38.584" x2="15.7226" y2="28.584"></line>
+                        </svg>
+                    </div>
+                    <div className="flex items-center">
+                        <p className="text-3xl font-medium text-gray-900 sm:text-4xl font-pj">2 000</p>
+                        <p className="ml-3 text-sm text-gray-900 font-pj">Patients<br />Actifs</p>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
